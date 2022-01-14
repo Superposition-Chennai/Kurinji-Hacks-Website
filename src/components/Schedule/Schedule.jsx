1a52f736e57d1a534e4930aad69a5540953be529
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2022-01-14 16:12:33 +0530

    site revamp

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -10,9 +10,7 @@ export default class Schedule extends React.PureComponent {
     return (
       <div className="schedule">
         <h1>Hackathon Schedule</h1>
-        <Paper>
-        <iframe className="airtable-embed" src="https://airtable.com/embed/shrvqCIYKWiqZFHRJ?backgroundColor=yellow&viewControls=on" frameBorder="0" width="100%" height="533" style={{background: "transparent", border: "1px solid #ccc"}}/>
-        </Paper>
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shrwlNXqRQOGtwpSP?backgroundColor=yellow" frameBorder="0" onmousewheel="" width="100%" height="100%" style={{background: "transparent", border: "1px solid #ccc"}}/>
       </div>
     );
   }
