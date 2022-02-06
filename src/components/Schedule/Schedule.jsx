153467a7eb27d20263297e34c47ddf791700acac
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2022-02-06 14:53:48 +0530

    prizes change

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -11,7 +11,7 @@ export default class Schedule extends React.PureComponent {
       <div className="schedule">
         <h1>Hackathon Schedule</h1>
         <center>
-        <iframe class="airtable-embed" src="https://airtable.com/embed/shrwlNXqRQOGtwpSP?backgroundColor=yellow&layout=card" frameBorder="0"style={{background: "transparent", border: "1px solid #ccc"}}/>
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shruOQ3xMB0kjeqTs?backgroundColor=yellow&viewControls=on" frameBorder="0"style={{background: "transparent", border: "1px solid #ccc"}}/>
         </center>
       </div>
     );
